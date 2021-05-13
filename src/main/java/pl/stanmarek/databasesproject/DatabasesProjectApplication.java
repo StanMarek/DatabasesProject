4a95ad7ebdbb98644342fc121586cde1ae09211a
StanMarek <stanislawmarek21@gmail.com>
2021-05-13 14:40:20 +0200

    Init

---
+++ src/main/java/pl/stanmarek/databasesproject/DatabasesProjectApplication.java
@@ -0,0 +1,14 @@
+package pl.stanmarek.databasesproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatabasesProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DatabasesProjectApplication.class, args);
+        System.out.println("hello");
+    }
+
+}
---
+++ src/test/java/pl/stanmarek/databasesproject/DatabasesProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.stanmarek.databasesproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatabasesProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
